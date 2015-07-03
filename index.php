b38727f4d3235b9aa571b636e9deb4e97eb398b2
Mélina Marignale <melina@marignale.com>
2015-07-03 16:25:31 +0200

    bootstrap

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html>
 <head lang="fr">
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="font/stylesheet.css"/>
     <link rel="stylesheet" href="css/style.css"/>
-    <title>Laura Capellier</title>
+    <title>BETMYPRESENCE.COM</title>
 </head>
     <body>
 <?php
